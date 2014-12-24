64e62a482db4f66f3c420dd73facac283cbb75a4
Tim <tpeterson@users.noreply.github.com>
2014-12-24 10:26:10 -0800

    Upload skeletal test of swapping between two images

---
+++ testslider.js
@@ -0,0 +1,9 @@
+var rightClick = function() {
+  document.getElementById("front").style.display="none";
+  document.getElementById("back").style.display="inline";
+}
+
+var leftClick = function() {
+  document.getElementById("front").style.display="inline";
+  document.getElementById("back").style.display="none";
+}
