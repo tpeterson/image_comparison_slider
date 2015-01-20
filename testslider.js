9d644bd3a65e7ce60bb2388a9ff1e1143053c60c
Tim <tpeterson@users.noreply.github.com>
2015-01-19 16:46:44 -0800

    Remove hiccup on overslide
    Slider no longer catches and sticks outside container when quickly slid
    past edge. Now slider catches at edge and stays there. Slider can catch
    at edge before image hits edge. When that happens, both catch with a
    gap. But when slider clicked and held, image snaps to slider.

--- testslider.js
+++ testslider.js
@@ -3,12 +3,10 @@ document.addEventListener("readystatechange", function(event) {
   window.addEventListener('mouseup', mouseUp, false);
 });
 
-// Stop dragging when slider button released
 function mouseUp() {
   window.removeEventListener('mousemove', dragItem, true);
 }
 
-// Call function to drag slider when mouse pressed down over slider button
 function mouseDown(e){
   window.addEventListener('mousemove', dragItem, true);
 }
@@ -18,7 +16,6 @@ function dragItem(e){
   var div = document.getElementById("slider");
   var containerPosition = document.getElementById("container").offsetLeft;
 
-  // TEST FOR BOUNDARIES ON SLIDER
   var containerWidth = document.getElementById("container").offsetWidth;
   var sliderWidth = document.getElementById("slider").offsetWidth;
   var minLeft = 0;
@@ -28,80 +25,33 @@ function dragItem(e){
   div.style.left = mouseleft - 15 + "px";
 
   slideImage(mouseleft, minLeft, maxLeft);
-
-  /*var sliderLeft = div.style.left;
-  console.log("Client X: " + e.clientX + "px");
-  console.log("Container: " + containerPosition + "px");
-  console.log("Container Width: " + containerWidth + "px");
-  console.log("MouseLeft: " + mouseleft + "px");
-  console.log("Slider Left: " + sliderLeft);
-  console.log("Min Left: " + minLeft + "px");
-  console.log("Max Left: " + maxLeft + "px");
-  console.log("Back: " + document.getElementById("back").offsetLeft);*/
 }
 
-// Function to slide back image in sync with slider button's location
 function slideImage(sliderLeft, minLeft, maxLeft) {
   var containerPosition = document.getElementById("container").offsetLeft;
   var containerWidth = document.getElementById("container").offsetWidth;
 
   var where = sliderLeft;
 
-  if (window.clientX > (containerPosition + containerWidth)) {
-    var div = document.getElementById("slider");
-    var leftedge = document.getElementById("front").style.width;
-    var containerWidth = document.getElementById("container").offsetWidth;
-    div.style.left = containerWidth - 15 + "px";
+  document.getElementById("front").style.width = where + "px";
 
-    where = containerWidth;
-
-    mouseUp();
-  }
-  else if (window.clientX < containerPosition) {
+  if (where < (minLeft - 15)) {
     var div = document.getElementById("slider");
     var leftedge = document.getElementById("front").style.width;
     var containerWidth = document.getElementById("container").offsetWidth;
     div.style.left = -15 + "px";
 
-    where = 0;
+    mouseUp();
+  }
+  else if (where > maxLeft + 15) {
+    var div = document.getElementById("slider");
+    var leftedge = document.getElementById("front").style.width;
+    var containerWidth = document.getElementById("container").offsetWidth;
+    div.style.left = containerWidth - 15 + "px";
 
     mouseUp();
   }
   else {
-    document.getElementById("front").style.width = where + "px";
+
   }
-
-  // TEST FOR BOUNDARIES ON SLIDER
-  /*if(where < minLeft) {
-  var div = document.getElementById("slider");
-  var leftedge = document.getElementById("front").style.width;
-  var containerWidth = document.getElementById("container").offsetWidth;
-  div.style.left = - 15 + "px";
-
-  where = 0;
-
-  mouseUp();
-} else if ( where > maxLeft) {
-var div = document.getElementById("slider");
-var leftedge = document.getElementById("front").style.width;
-var containerWidth = document.getElementById("container").offsetWidth;
-div.style.left = containerWidth - 15 + "px";
-
-where = containerWidth;
-
-mouseUp();
-}*
-else {
-document.getElementById("front").style.width = where + "px";
-}*/
-
-/*var frontPosition = document.getElementById("front").offsetWidth;
-var backPosition = document.getElementById("animal").offsetLeft;
-var sliderPosition = document.getElementById("slider").offsetLeft;
-var containerPosition = document.getElementById("container").offsetLeft;
-console.log("And now...");
-console.log("Front width: " + frontPosition);
-console.log("Back from left: " + backPosition);
-console.log("Slider from left: " + sliderPosition);
-console.log("Container from left: " + containerPosition);*/
 }
