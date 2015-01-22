9b297d2e003d47dfdec875b3ca0caf64a70ae4ec
Tim <tpeterson@users.noreply.github.com>
2015-01-22 14:19:32 -0800

    Finalized slider for Yahoo article
    Final template of the slider used for Yahoo article. Does not include
    tweaked CSS for #slider’s left positioning that was needed because of
    iframe weirdness on Ad Age site.

--- testslider.js
+++ testslider.js
@@ -1,6 +1,12 @@
 document.addEventListener("readystatechange", function(event) { 
   document.getElementById('slider').addEventListener('mousedown', mouseDown, false);
   window.addEventListener('mouseup', mouseUp, false);
+  
+  var leftedge = document.getElementById("front").offsetWidth;
+  var slider = document.getElementById("slider").offsetLeft;
+  
+  console.log(leftedge);
+  console.log(slider);
 });
 
 function mouseUp() {
@@ -22,7 +28,7 @@ function dragItem(e){
   var maxLeft = containerWidth;
   
   var mouseleft = e.clientX - containerPosition;
-  div.style.left = mouseleft - 15 + "px";
+  div.style.left = mouseleft - 20 + "px";
   
   slideImage(mouseleft, minLeft, maxLeft);
 }
@@ -35,19 +41,19 @@ function slideImage(sliderLeft, minLeft, maxLeft) {
 	
 	document.getElementById("front").style.width = where + "px";
 	
-	if (where < (minLeft - 15)) {
+	if (where < (minLeft - 20)) {
       var div = document.getElementById("slider");
 	  var leftedge = document.getElementById("front").style.width;
       var containerWidth = document.getElementById("container").offsetWidth;
-      div.style.left = -15 + "px"; 
+      div.style.left = - 20 + "px"; 
 
       mouseUp();
     }
-    else if (where > maxLeft + 15) {
+    else if (where > maxLeft + 20) {
       var div = document.getElementById("slider");
 	  var leftedge = document.getElementById("front").style.width;
       var containerWidth = document.getElementById("container").offsetWidth;
-      div.style.left = containerWidth - 15 + "px";
+      div.style.left = containerWidth - 20 + "px";
       
       mouseUp();
     }
