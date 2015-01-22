d9f68dea168fdb5d8c42980b004a19aec5adc7e5
Tim <tpeterson@users.noreply.github.com>
2015-01-22 14:13:51 -0800

    Customize slider for Yahoo article
    Tweaked the HTML, CSS and JS to accommodate the larger images used for
    the Yahoo article. Also added a vertical line to show divide between
    two images.

--- testslider.js
+++ testslider.js
@@ -1,57 +1,57 @@
-document.addEventListener("readystatechange", function(event) {
+document.addEventListener("readystatechange", function(event) { 
   document.getElementById('slider').addEventListener('mousedown', mouseDown, false);
   window.addEventListener('mouseup', mouseUp, false);
 });
 
 function mouseUp() {
-  window.removeEventListener('mousemove', dragItem, true);
+    window.removeEventListener('mousemove', dragItem, true);
 }
 
 function mouseDown(e){
-  window.addEventListener('mousemove', dragItem, true);
+    window.addEventListener('mousemove', dragItem, true);
 }
 
 // Function to drag slider
 function dragItem(e){
   var div = document.getElementById("slider");
   var containerPosition = document.getElementById("container").offsetLeft;
-
+  
   var containerWidth = document.getElementById("container").offsetWidth;
   var sliderWidth = document.getElementById("slider").offsetWidth;
-  var minLeft = 0;
+  var minLeft = 0; 
   var maxLeft = containerWidth;
-
+  
   var mouseleft = e.clientX - containerPosition;
   div.style.left = mouseleft - 15 + "px";
-
+  
   slideImage(mouseleft, minLeft, maxLeft);
 }
 
 function slideImage(sliderLeft, minLeft, maxLeft) {
-  var containerPosition = document.getElementById("container").offsetLeft;
-  var containerWidth = document.getElementById("container").offsetWidth;
+	var containerPosition = document.getElementById("container").offsetLeft;
+	var containerWidth = document.getElementById("container").offsetWidth;
 
-  var where = sliderLeft;
+	var where = sliderLeft;
+	
+	document.getElementById("front").style.width = where + "px";
+	
+	if (where < (minLeft - 15)) {
+      var div = document.getElementById("slider");
+	  var leftedge = document.getElementById("front").style.width;
+      var containerWidth = document.getElementById("container").offsetWidth;
+      div.style.left = -15 + "px"; 
 
-  document.getElementById("front").style.width = where + "px";
-
-  if (where < (minLeft - 15)) {
-    var div = document.getElementById("slider");
-    var leftedge = document.getElementById("front").style.width;
-    var containerWidth = document.getElementById("container").offsetWidth;
-    div.style.left = -15 + "px";
-
-    mouseUp();
-  }
-  else if (where > maxLeft + 15) {
-    var div = document.getElementById("slider");
-    var leftedge = document.getElementById("front").style.width;
-    var containerWidth = document.getElementById("container").offsetWidth;
-    div.style.left = containerWidth - 15 + "px";
-
-    mouseUp();
-  }
-  else {
-
-  }
+      mouseUp();
+    }
+    else if (where > maxLeft + 15) {
+      var div = document.getElementById("slider");
+	  var leftedge = document.getElementById("front").style.width;
+      var containerWidth = document.getElementById("container").offsetWidth;
+      div.style.left = containerWidth - 15 + "px";
+      
+      mouseUp();
+    }
+    else {
+   
+    }
 }
